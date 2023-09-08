2a3871deeec604382461fc2a41a6755514277e29
Your Name <you@example.com>
2023-09-08 13:57:36 -0700

    Loops in C

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry level
+ *
+ * Return: 0 (Successful)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = '0' ; a <= 15 ; a++)
+	{
+		if (a < 10)
+		{
+			putchar('0' + a);
+		}
+		else
+		{
+			putchar('A' + (a - 10));
+		}
+
+	}
+	putchar('\n');
+	return (0);
+}

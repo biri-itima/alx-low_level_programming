5328775899b9102da15006c440360119156b5ce0
Your Name <you@example.com>
2023-09-08 06:19:24 -0700

    Loops in C

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry level
+ *
+ * Return: 0 (Successful)
+ */
+int main(void)
+{
+	int digi;
+
+	for (digi = 0 ; digi <= 9 ; digi++)
+	printf("%d", digi);
+	putchar('\n');
+	return (0);
+}

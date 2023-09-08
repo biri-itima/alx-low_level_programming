c4ca750644294f4c6e27d17af14096a6461f185f
Your Name <you@example.com>
2023-09-08 05:54:56 -0700

    Loops in C

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry level
+ *
+ * Return: 0 (Successful)
+ */
+int main(void)
+{
+	char excq_e;
+
+	for (excq_e = 'a' ; excq_e <= 'z'; excq_e++)
+	{
+		if (excq_e != 'e' && excq_e != 'q')
+		{
+			putchar(excq_e);
+			putchar('\n');
+		}
+	}
+	return (0);
+}

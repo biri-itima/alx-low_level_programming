e0e953c959e9a928b890419169ea6f58ea23d214
Your Name <you@example.com>
2023-09-08 06:26:41 -0700

    Loop in c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry level
+ *
+ * Return: 0 (Successful)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0' ; n <= '9' ; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}

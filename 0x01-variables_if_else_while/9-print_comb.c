dbd54e34bfc892ca5fd5f89d974a2e88c01a85ea
Your Name <you@example.com>
2023-09-08 23:58:38 -0700

    Loops in C

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,14 +12,13 @@ int main(void)
 
 	for (a = 0 ; a <= 9 ; a++)
 	{
-		if (a == 9)
-			putchar(a + '0');
-		else
+		putchar(a + '0');
+		if (a != 9)
 		{
-			putchar(a + '0');
 			putchar(',');
 			putchar(' ');
 		}
 	}
+	putchar('\n');
 	return (0);
 }

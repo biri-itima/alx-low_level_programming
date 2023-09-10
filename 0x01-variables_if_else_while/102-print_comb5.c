2f8d05d8d32810ead0acd7f05a20f065f71d7d01
Your Name <you@example.com>
2023-09-10 09:56:40 -0700

    Loops in C

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: 0 (Successful)
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int a1;
+	int a2;
+	int b1;
+	int b2;
+
+	for (a = 0; a <= 99; a++)
+	{
+		for (b = a; b <= 99; b++)
+		{
+			a1 = a / 10;
+			a2 = a % 10;
+			b1 = b / 10;
+			b2 = b % 10;
+
+			putchar(a1 + '0');
+			putchar(a2 + '0');
+			putchar(' ');
+			putchar(b1 + '0');
+			putchar(b2 + '0');
+
+			if (a != 99 || b != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
+

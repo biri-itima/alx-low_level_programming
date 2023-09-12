5981363df7c54570c75b0302087c323ef16db5ed
Your Name <you@example.com>
2023-09-12 05:15:36 -0700

    Loops in C Project

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,14 +8,13 @@ void print_alphabet_x10(void)
 {
 	int num;
 
-	char a;
+	int a;
 
 	for (num = 0; num <= 10; num++)
 	{
 		for (a = 'a'; a <= 'z'; a++)
-		{
 			_putchar(a);
-		}
+		
 		_putchar('\n');
 	}
 }

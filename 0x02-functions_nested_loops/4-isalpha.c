6d7a8b9447628673708d0575f4a3a9e663933ead
Your Name <you@example.com>
2023-09-12 08:07:18 -0700

    Loops

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - to check if it is an alphabet
+ * @c: is a letter upper or lowercase
+ *
+ * Return: 1 or 0
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}

a93757778ba8d2402e4cf101a5c31c234bb28ba5
Your Name <you@example.com>
2023-09-13 00:20:11 -0700

    LOOPS

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,16 +8,15 @@
  */
 void print_to_98(int n)
 {
-
 	int i;
-	
+
 	for (i = n; i <= 98; i++)
 	{
 		if (n < 0)
 			printf("%d, ", i);
 		else if (n > 0)
 		{
-			for (i = n; i >= 98; i--)
+			for (i = n; i <= 98; i++)
 			printf("%d, ", i);
 		}
 	}

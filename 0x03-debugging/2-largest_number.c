048095ce43ecd39ee48d515e1063f0b5dc7a6223
Your Name <you@example.com>
2023-09-14 01:13:30 -0700

    mDebugging

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -15,17 +15,14 @@ int largest_number(int a, int b, int c)
 	if (a > b && a > c)
 	{
 		largest = a;
-		printf("\n");
 	}
 	else if (b > a && b > c)
 	{
 		largest = b;
-		printf("\n");
 	}
-	else
+	else if (c > a && c > b)
 	{
 		largest = c;
-		printf("\n");
 	}
 	return (largest);
 }

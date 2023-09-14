a3327020aa53ddbe2217be7a3a9e2ce4b2e7e1e1
Your Name <you@example.com>
2023-09-14 06:35:54 -0700

    More functions

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - Multiply two integers
+ *
+ * @a: Parameters 1
+ * @b: Parameter 2
+ *
+ * Return: 0 successful
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <stdio.h>
-
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);

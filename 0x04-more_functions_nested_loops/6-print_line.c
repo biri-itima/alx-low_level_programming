48e8aa7daa66a816500d3ad65fe216877b7d835d
Your Name <you@example.com>
2023-09-14 11:11:25 -0700

    More functions

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_line - print straight line
+ *
+ * @n: parameter
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+		else if (n > 0)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}

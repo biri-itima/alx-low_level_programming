167b8f4d297373428a670d6220eea50cba722526
Your Name <you@example.com>
2023-09-14 21:04:31 -0700

    More functions

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,18 +6,21 @@
  */
 void print_square(int size)
 {
-	int a;
+	int a, b;
 
-	for (a = 0; a <= size; a++)
+	if (size > 0)
 	{
-		if (size > 0)
+	for (a = 0; a < size; a++)
+	{
+		for (b = 0; b < size; b++)
 		{
 			_putchar('#');
 		}
+		_putchar('\n');
+	}
+	}
 		else if (size <= 0)
 		{
 			_putchar('\n');
 		}
-	}
-	_putchar('\n');
 }

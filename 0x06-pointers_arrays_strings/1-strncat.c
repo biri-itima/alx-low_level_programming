6de066d8672c5bc371ffb9a348176d6a2d9be2a7
Your Name <you@example.com>
2023-09-20 09:14:44 -0700

    More pointers and arrays

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,12 +10,12 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int dest_len = 0;
+	int dest_len = strlen(dest);
 	int i;
 
 	for (i = 0; i < n && *src != '\0'; i++)
 	{
-		dest[dest_len + i] = src[i];
+		dest[dest_len + i] = *src;
 		src++;
 	}
 	dest[dest_len + i] = '\0';

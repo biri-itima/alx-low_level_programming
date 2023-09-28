090700f6b76d18d0a14506769db184dde54aa576
Your Name <you@example.com>
2023-09-28 08:37:26 -0700

    Recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,23 +1,32 @@
 #include "main.h"
 
 /**
- * is_palindrome - check if it is a palindrome
+ * is_palindrome_rec - check if it is a palindrome
  *
  * @s: parameter
+ * @str: begining char
+ * @end: end char
  *
  * Return: 1
  */
+int is_palindrome_rec(char *s, int str, int end)
+{
+	if (str >= end)
+	{
+		return (1);
+	}
+
+	else if (s[str] != s[end])
+	{
+		return (0);
+	}
+
+	return (is_palindrome_rec(s, str + 1, end - 1));
+}
+
 int is_palindrome(char *s)
 {
-	int length = strlen(s);
-	int i, j;
+	int l = strlen(s);
 
-	for (i = 0, j = length - 1; i < j; i++, j--)
-	{
-		if (s[i] != s[j])
-		{
-			return (0);
-		}
-	}
-	return (1);
+	return (is_palindrome_rec(s, 0, l - 1));
 }

d6b003f3f55b0a2b0ecfbf83d5783d69ccd11597
Your Name <you@example.com>
2023-10-02 13:01:30 -0700

    Argc Argv

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 (successfully)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for(i = 0; i <= argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}

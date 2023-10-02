e9f9780496305abfc5da95b52ca4890e581f66b4
Your Name <you@example.com>
2023-10-02 12:44:59 -0700

    Argc Argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: Number or arg
+ * @argv: array of arg
+ * Return: 0 successfully
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+
+	return (0);
+}

6759c32a649f5d3d635def4ed8bb2be6c1b1d014
Your Name <you@example.com>
2023-10-04 02:05:45 -0700

    Malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,9 +19,9 @@ char *str_concat(char *s1, char *s2)
 	total_len = len_s1 + len_s2 + 1;
 
 	if (s1 == NULL)
-		s1 = "";
+		s1 = " ";
 	if (s2 == NULL)
-		s2 = "";
+		s2 = " ";
 
 	cat_arr = (char *)malloc(total_len);
 

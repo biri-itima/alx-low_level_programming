3424035ed162eea3d1d22a45efdb5a8421d1c49e
Your Name <you@example.com>
2023-10-04 02:17:07 -0700

    Malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,16 +18,16 @@ char *str_concat(char *s1, char *s2)
 	{
 	len_s1 = strlen(s1);
 	}
+	else
+		s1 = " ";
 	if (s2 != NULL)
 	{
 	len_s2 = strlen(s2);
 	}
-	total_len = len_s1 + len_s2 + 1;
-
-	if (s1 == NULL)
-		s1 = " ";
-	if (s2 == NULL)
+	else
 		s2 = " ";
+
+	total_len = len_s1 + len_s2 + 1;
 
 	cat_arr = (char *)malloc(total_len);
 

f359e8a24a868d7de7db34a6d76ea8c1dddb9c47
Your Name <you@example.com>
2023-10-24 10:13:45 -0700

    singly linked list

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - insert at index
+ * @head: head of the list
+ * @idx: index the node will be add
+ * @n: int
+ * Return: New node
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i;
+	listint_t *new_node;
+	listint_t *current;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->n = n;
+
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+
+	current = *head;
+	for (i = 0; i < idx - 1 && current != NULL; i++)
+	{
+		current = current->next;
+	}
+	if (current == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->next = current->next;
+	current->next = new_node;
+
+	return (new_node);
+
+}

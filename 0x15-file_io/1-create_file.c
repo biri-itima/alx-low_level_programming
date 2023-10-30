3140dcab6729c37fdb0f03873c9487baabb40fa5
Your Name <you@example.com>
2023-10-30 12:29:51 -0700

    correction

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,30 +7,22 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int f;
-	size_t text_len;
-	ssize_t lenw;
+	int f, text_len, rdwr;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
-	f = open(filename, O_WRONLY | O_CREAT, 0600);
-
+	f = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (f == -1)
 		return (-1);
+	if (!text_content)
+		text_content = "";
+	for (text_len = 0; text_content[text_len]; text_len++)
+		;
+	rdwr = write(f, text_content, text_len);
 
-	if (text_content != NULL)
-	{
-		text_len = strlen(text_content);
-		lenw = write(f, text_content, text_len);
-
-		if (lenw == -1)
-		{
-			close(f);
-			return (-1);
-		}
-	}
+	if (rdwr == -1)
+		return (-1);
 	close(f);
-
 	return (1);
 }
